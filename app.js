c9e986ec52d5be505c178b9a9ba9afef2e8cece0
Harsh Ambastha <harshambastha0204@gmail.com>
2024-11-14 19:19:47 +0530

    Updated the cli working from readline to process.argv

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const readline = require('node:readline');
-
 const addTask = require('./commands/add');
 const printTask = require('./commands/list');
 const markInProgress = require('./commands/mark_in_progress');
@@ -7,42 +5,30 @@ const markDone = require('./commands/mark_done');
 const deleteTask = require('./commands/delete');
 const updateTask = require('./commands/update');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const command = process.argv;
+const arg1 = command[2];
+const arg2 = command[3];
+const arg3 = command[4];
 
-
-rl.on('line', (input)=>{
-    const splitInputArray = input.split(' ');
-    const command = splitInputArray[0];
-    const argument = splitInputArray[1];
-    const argument1 = splitInputArray[2];
-
-    switch(command){
-        case 'add':
-            addTask(argument);
-            break;
-        case 'list':
-            printTask(argument);
-            break;
-        case 'mark-in-progress':``
-            markInProgress(argument);
-            break;
-        case 'mark-done':
-            markDone(argument);
-            break;
-        case 'delete':
-            deleteTask(argument);
-            break;
-        case 'update':
-            updateTask(argument,argument1);
-            break;
-        case 'exit':
-            console.log('Bye, Have a nice day');
-            rl.close();
-            break;
-        default:
-            console.log('Not a valid command');
-    }
-})
+switch (arg1) {
+    case 'add':
+        addTask(arg2);
+        break;
+    case 'list':
+        printTask(arg2);
+        break;
+    case 'mark-in-progress':
+        markInProgress(arg2);
+        break;
+    case 'mark-done':
+        markDone(arg2);
+        break;
+    case 'delete':
+        deleteTask(arg2);
+        break;
+    case 'update':
+        updateTask(arg2, arg3);
+        break;
+    default:
+        console.log('Not a valid command');
+}
